7235a39f6257c119b0da6b5ad9bf2c02fe86fd36
GOE-skater <tomatoaug27@gmail.com>
2026-06-04 17:59:56 +0900

    Update files

--- include/arrays.hpp
+++ include/arrays.hpp
@@ -601,6 +601,7 @@ struct GridInterfaceR{
         //--------------------------------
         i_flr_bl(ni+2,vector<int>(nj+2,0.0)),
         j_flr_bl(ni+2,vector<int>(nj+2,0.0)),
+        
         //--------------------------------
 
         //index-convergion
